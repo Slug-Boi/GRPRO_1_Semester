ae70c9ec7d406c5985188fc05686d2a976f780c6
Theis <112340858+Slug-Boi@users.noreply.github.com>
2022-09-05 21:20:25 +0200

    Add files via upload

---
+++ KattisProjekter/Kattis_Opgaver/Stuck_Time.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Stuck_Time {
+    public static void main(String[] args) {
+        Scanner count = new Scanner(System.in);
+        
+        int input = count.nextInt();
+        for (int i = 1 ; i <= input; i++) {
+            System.out.println(i + " Abracadabra");
+        }
+        
+    }
+}
