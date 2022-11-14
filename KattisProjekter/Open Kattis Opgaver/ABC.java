78b56bdbeca3e406ded95962b2636fbd2e384970
Per Holm <karpe@MacBook-Pro.local>
2022-11-14 10:57:48 +0100

    "Changed ABC to be right file"

--- KattisProjekter/Open Kattis Opgaver/ABC.java
+++ KattisProjekter/Open Kattis Opgaver/ABC.java
@@ -1,32 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
 public class ABC {
     public static void main(String[] args) {
-        //ABC, ACB, CBA, BAC, BCA
         Scanner sc = new Scanner(System.in);
-        int A = 0;
-        int B = 0;
-        int C = 0;
-        int [] array = new int[3];
-        for(int i = 0 ; i < 2 ; i++) {
-            array[i] = sc.nextInt();
-        }
-        String abc = sc.nextLine();
-        for(int i = 0 ; i < 2 ; i++) {
-            if(array[i] > A){
-                A = array[i];
+        List<Integer> numbers = new ArrayList<>();
+        List<String> letters = new ArrayList<>();
+
+        String inputNum = sc.nextLine();
+        String[] splitNum = inputNum.split(" ");
+
+
+        numbers.add(Integer.parseInt(splitNum[0]));
+        numbers.add(Integer.parseInt(splitNum[1]));
+        numbers.add(Integer.parseInt(splitNum[2]));
+        Collections.sort(numbers);
+
+        String inputStr = sc.nextLine();
+        String[] splitStr = inputStr.split("");
+        letters.add(splitStr[0]);
+        letters.add(splitStr[1]);
+        letters.add(splitStr[2]);
+
+        for(int i = 0 ; i < 3 ; i++) {
+            if(letters.get(i).equals("A")) {
+                System.out.print(numbers.get(0));
+            } else if(letters.get(i).equals("B")) {
+                System.out.print(numbers.get(1));
+            } else {
+                System.out.print(numbers.get(2));
             }
+            System.out.print(" ");
         }
-        for(int i = 0 ; i < 2 ; i++) {
-            if(array[i] < A){
-                C = array[i];
-            }
-        }
-        for(int i = 0 ; i < 2 ; i++) {
-            if(array[i] < A && array[i] > B){
-                B = array[i];
-            }
-        }
-        String abc = sc.nextLine();
-        
     }
 }
